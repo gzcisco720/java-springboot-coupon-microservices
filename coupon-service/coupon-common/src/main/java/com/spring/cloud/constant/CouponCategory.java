02063ee80abf32db815f67163ff5d9af8ed12255
ericgong <eric@192-168-1-101.tpgi.com.au>
2020-05-09 19:32:34 +1000

    feat(constant): create constants

---
+++ coupon-service/coupon-common/src/main/java/com/spring/cloud/constant/CouponCategory.java
@@ -0,0 +1,28 @@
+package com.spring.cloud.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+public enum CouponCategory {
+
+    REBATE("rebate", "001"),
+    DISCOUNT("discount","002"),
+    DEDUCTION("deduction", "003");
+
+    private String description;
+
+    private String code;
+
+    public static CouponCategory of(String code) {
+        Objects.requireNonNull(code);
+        return Stream.of(values())
+                .filter(bean -> bean.code.equals(code))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(code + " not exists"));
+    }
+}
---
+++ coupon-service/coupon-common/src/main/java/com/spring/cloud/constant/ProductLine.java
@@ -0,0 +1,27 @@
+package com.spring.cloud.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+public enum ProductLine {
+
+    ONE("line 1",1),
+    TWO("line 2",2);
+
+    private String description;
+
+    private Integer code;
+
+    public static ProductLine of(Integer code) {
+        Objects.requireNonNull(code);
+        return Stream.of(values())
+                .filter(bean -> bean.code.equals(code))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(code + " not exists"));
+    }
+}
